301da27f69dfd072cb03d1057020eb4d612a636c
Alejandro García Cortado <alejandrogarciacortado@gmail.com>
2018-05-09 13:51:34 +0200

    Cambios

---
+++ Boletin3/alta.h
@@ -0,0 +1,18 @@
+#ifndef ALTA_H_INCLUDED
+#define ALTA_H_INCLUDED
+#include "cliente.h"
+
+/**
+\file alta.h
+\brief Archivo con las funciones para dar de alta a los usuarios y sus viajes
+\author José Maria Mirete Fernandez
+\author Alejandro García Cortado
+\version 1.0
+\date 04/2018
+*/
+clientes *alta_cliente(int *contador_clientes, clientes *cliente);
+clientes *alta_viaje(int contador_clientes,clientes *cliente);
+#endif // ALTA_H_INCLUDED
+
+
+
---
+++ Boletin3/menu.h
@@ -0,0 +1,15 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+/**
+\file menu.h
+\brief Archivo con el menú del programa
+\author José Maria Mirete Fernandez
+\author Alejandro García Cortado
+\version 1.0
+\date 04/2018
+*/
+
+void menu();
+
+#endif // MENU_H_INCLUDED
+
